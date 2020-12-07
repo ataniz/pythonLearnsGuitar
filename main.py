a5724b8119624e22819201909f9892d95438d334
Ata Nizamoglu <nizamoglu@campus.tu-berlin.de>
2020-12-07 16:32:04 +0100

    scales are scraped from wikipedia

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 import numpy as np
+# import scrape
 import matplotlib.pyplot as plt
 
 # adding tuning would be kinda cool too
 
 notes = np.array(['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'] * 3)
-
-scales = {
-
-}
+scales = {'Acoustic scale': [0, 2, 4, 6, 7, 9, 10, 12],
+          'Aeolian mode or natural minor scale': [0, 2, 3, 5, 7, 8, 10, 12],
+          'Algerian scale': [0, 2, 3, 6, 7, 8, 11, 12, 14, 15, 17],
+          'Altered scale or Super Locrian scale': [0, 1, 3, 4, 6, 8, 10, 12],
+          'Augmented scale': [0, 3, 4, 7, 8, 11, 12], 'Bebop dominant scale': [0, 2, 4, 5, 7, 9, 10, 11, 12],
+          'Blues scale': [0, 3, 5, 6, 7, 10, 12], 'Chromatic scale': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+          'Dorian mode': [0, 2, 3, 5, 7, 9, 10, 12], 'Double harmonic scale': [0, 1, 4, 5, 7, 8, 11, 12],
+          'Enigmatic scale': [0, 1, 4, 6, 8, 10, 11, 12], 'Flamenco mode': [0, 1, 4, 5, 7, 8, 11, 12],
+          '"Gypsy" scale': [0, 2, 3, 6, 7, 8, 10, 12], 'Half diminished scale': [0, 2, 3, 5, 6, 8, 10, 12],
+          'Harmonic major scale': [0, 2, 4, 5, 7, 8, 11, 12], 'Harmonic minor scale': [0, 2, 3, 5, 7, 8, 11, 12],
+          'Hirajoshi scale': [0, 4, 6, 7, 11, 12],
+          'Hungarian "Gypsy" scale/Hungarian minor scale': [0, 2, 3, 6, 7, 8, 11, 12],
+          'Hungarian major scale': [0, 3, 4, 6, 7, 9, 10, 12], 'In scale': [0, 1, 5, 7, 8, 12],
+          'Insen scale': [0, 1, 5, 7, 11, 13], 'Ionian mode or major scale': [0, 2, 4, 5, 7, 9, 11, 12],
+          'Istrian scale': [0, 1, 3, 4, 6, 7], 'Iwato scale': [0, 1, 5, 6, 10, 12],
+          'Locrian mode': [0, 1, 3, 5, 6, 8, 10, 12], 'Lydian augmented scale': [0, 2, 4, 6, 8, 9, 11, 12],
+          'Lydian mode': [0, 2, 4, 6, 7, 9, 11, 12], 'Major bebop scale': [0, 2, 4, 5, 7, 8, 9, 11, 12],
+          'Major Locrian scale': [0, 2, 4, 5, 6, 8, 10, 12], 'Major pentatonic scale': [0, 2, 4, 7, 9, 12],
+          'Melodic minor scale (ascending)': [0, 2, 3, 5, 7, 9, 11, 12], 'Minor pentatonic scale': [0, 3, 5, 7, 10, 12],
+          'Mixolydian mode or Adonai malakh mode': [0, 2, 4, 5, 7, 9, 10, 12],
+          'Neapolitan major scale': [0, 1, 3, 5, 7, 9, 11, 12], 'Neapolitan minor scale': [0, 1, 3, 5, 7, 8, 11, 12],
+          'Persian scale': [0, 1, 4, 5, 6, 8, 11, 12], 'Phrygian dominant scale': [0, 1, 4, 5, 7, 8, 10, 12],
+          'Phrygian mode': [0, 1, 3, 5, 7, 8, 10, 12], 'Prometheus scale': [0, 2, 4, 6, 9, 10, 12],
+          'Scale of harmonics': [0, 3, 4, 5, 7, 9, 12], 'Tritone scale': [0, 1, 4, 6, 7, 10, 12],
+          'Two-semitone tritone scale': [0, 1, 2, 6, 7, 8], 'Ukrainian Dorian scale': [0, 2, 3, 6, 7, 9, 10, 12],
+          'Whole tone scale': [0, 2, 4, 6, 8, 10, 12], 'Yo scale': [0, 3, 5, 7, 10, 12],
+          'Melodic minor scale (descending)': [0, 2, 4, 5, 7, 9, 10, 12],
+          'Octatonic scale (Mode 1)': [0, 2, 3, 5, 6, 8, 9, 11, 12],
+          'Octatonic scale (Mode 2)': [0, 1, 3, 4, 6, 7, 9, 10]}
 
 
 def get_notes(indices):
@@ -15,4 +41,8 @@ def get_notes(indices):
     indices = np.array(indices) - 1
     return np.take(notes, indices)
 
-# def get_scales(scale, root):
+
+def get_scale(root, scale):
+    if __name__ == '__main__':
+        # scales = scrape.get_scales()
+        print(scales)
---
+++ scrape.py
@@ -0,0 +1,67 @@
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+import numpy as np
+
+
+# getting scales from wikipedia with some webscraping
+#  get name, intervals
+#  convert interval values( W,H,3H) to steps as in H=1 W=2 (one step is a semitone)
+
+def get_scales_from_wiki():
+    url = 'https://en.wikipedia.org/wiki/List_of_musical_scales_and_modes'
+    res = requests.get(url)
+
+    soup = BeautifulSoup(res.text, "html.parser")
+    rows = soup.findAll('tr')
+    scales = dict()
+
+    for i, row in enumerate(rows[1:66]):
+        columns = row.findAll('td')
+        scale_name = columns[0].text
+        intervals = columns[4].text
+        if intervals.strip() == "—" or "Q" in intervals:  # skipping scales with no interval values or with quarter notes
+            continue
+        scales[scale_name] = intervals.strip().replace("(", "").replace(")", "")
+    return scales
+
+
+# seems like melodic minor has  a descending and an ascending value
+# octatonic has two modes
+# Istrian scale has 2 notes missing
+def fix_melodic_and_octatonic(scales):
+    interval = scales.pop('Melodic minor scale')
+    scales['Melodic minor scale (descending)'] = interval.split('\n')[1][:13]
+    intervals = scales.pop('Octatonic scale')
+    scales['Octatonic scale (Mode 1)'] = intervals.split('\n\n')[0]
+    scales['Octatonic scale (Mode 2)'] = intervals.split('\n\n')[1]
+    scales['Istrian scale'] = 'H-W-H-W-H'
+    return scales
+
+def convert2step(note):
+    if note == 'H':
+        return 1
+    elif note == "W":
+        return 2
+    else:
+        coef = int(note[0])
+        return coef*convert2step(note[1])
+
+def notes2steps(scales):
+    scales_with_steps = dict()
+    for scale in scales:
+        intervals = scales[scale]
+        steps = [0]
+        for note in intervals.split('-'):
+            base = steps[-1]
+            steps.append(base + convert2step(note))
+        scales_with_steps[scale] = steps
+    return scales_with_steps
+
+def get_scales():
+    wiki_scales = get_scales_from_wiki()
+    fixed_scales = fix_melodic_and_octatonic(wiki_scales)
+    scales_with_steps = notes2steps(fixed_scales)
+    return scales_with_steps
+
